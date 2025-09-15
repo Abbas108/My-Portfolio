58f11ceeb30d419a2dad8daa320c68a9e705514e
Abbas108 <86660389+Abbas108@users.noreply.github.com>
2025-09-15 22:42:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const projects = [
    // price: "$500",
   
     images: [
-      { src: "images/qshop.jpg", desc: "Main Page", descAr: "الصفحة الأساسية" },
+      { src: "images/delivery/qshop.jpg", desc: "Main Page", descAr: "الصفحة الأساسية" },
     
     ]
   
@@ -135,3 +135,4 @@ document.getElementById("langBtn").addEventListener("click", toggleLanguage);
 
 // التحميل الأولي
 document.addEventListener("DOMContentLoaded", () => loadProjects());
+
